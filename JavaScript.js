369f8378414adf6c81fed8d829dcd82410e60b79
burroughclarke <burroughclarkesa@gmail.com>
2017-03-25 19:53:08 +1030

    -added test 'persisting storage' page & javascript

--- JavaScript.js
+++ JavaScript.js
@@ -150,10 +150,9 @@ function verifyEmail()
         }
         
 
-
-        function titleTextClick()
+        function getDate()
         //credit to http://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
-        {            
+        {
             var today = new Date();
             var dd = today.getDate();
             var mm = today.getMonth()+1; //January is 0!
@@ -169,10 +168,98 @@ function verifyEmail()
 
             today = mm+ '/' + dd +'/'+ yyyy;
             
+            return today;
+        }
+
+
+        function titleTextClick()
+        {            
+            var today = getDate();   
             document.getElementById('titleText').placeholder = "(Default title: " + today + ")";
         }
         
         function journalTextClick()
         {
              document.getElementById('journalText').placeholder = "What happened today? Start at the beginning...";
+        }
+
+
+
+        function testSet()
+        {
+//            sessionStorage.getItem('content1'); //needed??
+            sessionStorage.setItem('content1', document.getElementById('journalText').value);
+            
+            if (document.getElementById('titleText').value == "")
+                {
+                    sessionStorage.setItem('title1', getDate());
+
+                }
+            else
+                {
+                    sessionStorage.setItem('title1', document.getElementById('titleText').value);
+                }
+            
+            if (document.getElementById('journalText').style.fontWeight == "bold")
+                {
+                    sessionStorage.setItem("bold1", "true");
+                }
+            else
+                {
+                  sessionStorage.setItem("bold1", "false");
+                }
+            
+            if (document.getElementById('journalText').style.fontStyle == "italic")
+                {
+                    sessionStorage.setItem("italic1", "true");
+                }
+            else
+                {
+                  sessionStorage.setItem("italic1", "false");
+                }
+            
+            if (document.getElementById('journalText').style.textDecoration == "underline")
+                {
+                    sessionStorage.setItem("underline1", "true");
+                }
+            else
+                {
+                  sessionStorage.setItem("underline1", "false");
+                }
+            
+            
+            sessionStorage.setItem("fontColor1",  document.getElementById('journalText').style.color);
+            sessionStorage.setItem("fontSize1",   document.getElementById('journalText').style.fontSize);
+            sessionStorage.setItem("fontFamily1", document.getElementById('journalText').style.fontFamily);
+
+            
+        }
+
+
+//
+        function loadPastEntries()
+        {
+            document.getElementById('history1').value = sessionStorage.getItem('content1');
+            document.getElementById('title1').value = sessionStorage.getItem('title1');
+            
+            if (sessionStorage.getItem("bold1") == "true")
+                {
+                    document.getElementById("history1").style.fontWeight = "bold";
+                }
+            
+            if (sessionStorage.getItem("italic1") == "true")
+                {
+                    document.getElementById("history1").style.fontStyle = "italic";
+                }
+            
+            if (sessionStorage.getItem("underline1") == "true")
+                {
+                    document.getElementById("history1").style.textDecoration = "underline";
+                }
+            
+            document.getElementById("history1").style.color = sessionStorage.getItem('fontColor1');
+            document.getElementById("history1").style.fontSize = sessionStorage.getItem('fontSize1');
+            document.getElementById("history1").style.fontFamily = sessionStorage.getItem('fontFamily1');
+
+            
         }
