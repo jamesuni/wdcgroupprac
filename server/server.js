65310fdc6e638600c12aa8d708112f4427fc1a52
Jack <fyruxis@gmail.com>
2017-04-15 11:18:33 +0930

    Added server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+// server.js runs content serving at a given port
+
+var port = 8080;
+
+const express = require('express');         // express framework for server
+const bodyParser= require('body-parser');   // form handling
+const path = require('path');               // path handling
+const favicon = require('serve-favicon');   // favicon handling
+const app = express();                      // create server
+
+// set favicon
+//app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
+app.use('/favicon.ico', express.static('images/favicon.ico'));
+// app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+// read from form submission
+app.use(bodyParser.urlencoded({extended: true}));
+
+// static file handling and virtual path
+//  all statics files will automatically be requested
+app.use(express.static(path.join(__dirname, 'public/css')));
+app.use(express.static(path.join(__dirname, 'public/images')));
+app.use(express.static(path.join(__dirname, 'public/js')));
+
+// start server
+app.listen(port, function() {
+  console.log('Listening on ' + port)
+})
+
+// default page is index.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+console.log('Server running');
