53f6eae19aa66ff0f4a1b4630a559e8afdc13a5a
burroughclarke <burroughclarkesa@gmail.com>
2017-04-21 19:36:37 +0930

    -custom title option

--- js/account.js
+++ js/account.js
@@ -104,33 +104,142 @@
 //
 //}
 
-    // type 'password' is causing it to block actions, like 'alert(pasword)'  
-    //    else if (document.getElementById("paswordText").value == "")
-    //    {
-    //                document.getElementById("submitStatus").style.color = "red";
-    //                document.getElementById("submitStatus").innerHTML = "Submission error: enter your password";
-    //    }
+//the fact it is type 'password' is causing it to block actions, like 'alert(pasword)'  
+//    else if (document.getElementById("paswordText").value == "")
+//    {
+//                document.getElementById("submitStatus").style.color = "red";
+//                document.getElementById("submitStatus").innerHTML = "Submission error: enter your password";
+//    }
 
 
-function loadLogin() {
-    "use strict";
-    
-    if (sessionStorage.getItem('emailText') !== null) {
-        document.getElementById("loginBox").innerHTML = "Logged in as: [" + sessionStorage.getItem('emailText') + "]";
-        document.getElementById("loginBox").style.color = "blue";
+//set default color, size and style of journal text for new.html
 
-
-        var element = document.createElement("button"),
-            foo = document.getElementById("loginBox");
-
-
-        element.innerText = "Logout";
-        element.id = "logout";
-        element.onclick = function () {
-            sessionStorage.removeItem("emailText");
-            sessionStorage.removeItem("passwordText");
-            window.location.reload(false);
-        };
-        foo.appendChild(element);
+//this function could actually be responsible for loadimg all default settings.
+function loadBackgroundColor() {
+    if (localStorage.getItem("backgroundColor") != null) {
+        console.log("backgroundColor: " + localStorage.getItem("backgroundColor"));
+        document.body.style.background = localStorage.getItem("backgroundColor");
     }
+
+    if (localStorage.getItem("textColor") != null) {
+        console.log("textColor: " + localStorage.getItem("textColor"));
+
+        for (var i = 0; i < document.getElementsByTagName("h2").length; i += 1) {
+            var title = document.getElementsByTagName("h2")[i];
+            //            title.style.font = "Impact"; //why is 'font' not working??
+            title.style.color = localStorage.getItem("textColor");
+
+        }
+
+        for (var i = 0; i < document.getElementsByTagName("h1").length; i += 1) {
+            var title = document.getElementsByTagName("h1")[i];
+            //            title.style.font = "italic bold 20px";
+            title.style.color = localStorage.getItem("textColor");
+
+        }
+        //        document.body.style.background = localStorage.getItem("textColor");
+    }
+
+    var title = document.getElementById("pageTitle");
+    
+    if (localStorage.getItem("titleStyle") == "retro")
+        {
+                title.textContent = "Ye Olde Printingus Pressus";
+                title.style.fontSize = "50px";
+                title.style.textDecoration = "underline";
+                title.style.fontStyle = "normal";
+                title.style.color = localStorage.getItem("textColor");
+        }
+    if (localStorage.getItem("titleStyle") == "futuristic")
+        {
+                title.textContent = "iJournal 2.0 [Beta]";
+                title.style.fontSize = "50px";
+                title.style.fontStyle = "italic";
+                title.style.color = localStorage.getItem("textColor");
+        }
+    if (localStorage.getItem("titleStyle") == "normal")
+        {
+                title.textContent = "Journal Website";
+                title.style.fontSize = "50px";
+                title.style.fontStyle = "normal";
+                title.style.color = localStorage.getItem("textColor");
+        }
+
+    for (var i = 0; i < document.getElementsByTagName('nav').length; i += 1)
+        {
+            var button = document.getElementsByTagName("nav")[i];
+            button.style.color = localStorage.getItem("textColor");
+        }
+    
+}//func
+
+function selectDefaultStyle() {
+    //    ....TODO
 }
+
+//select default background color for style.css
+function selectBackground() {
+    //    alert("selectBackground");
+    document.body.style.background = document.getElementById("selectBackground").value;
+    console.log("document background color changed to: " + document.getElementById("selectBackground").value);
+    localStorage.setItem("backgroundColor", document.getElementById("selectBackground").value);
+}
+
+function selectTextColor() {
+    console.log("selectTextColor");
+    for (var i = 0; i < document.getElementsByTagName("h2").length; i += 1) {
+        var title = document.getElementsByTagName("h2")[i];
+        //            title.style.font = "Impact"; //why is 'font' not working??
+        title.style.color = document.getElementById("selectTextColor").value;
+
+    }
+    for (var i = 0; i < document.getElementsByTagName("h1").length; i += 1) {
+        var title = document.getElementsByTagName("h1")[i];
+        //            title.style.font = "italic bold 20px";
+        title.style.color = document.getElementById("selectTextColor").value;
+
+    }
+
+    localStorage.setItem("textColor", document.getElementById("selectTextColor").value);
+
+    //    document.getElementsByTagName("h1").style.font = "Sansita One";
+    //    alert("selectTitle");
+}
+
+function selectTitle() {
+
+    
+    var title = document.getElementById("pageTitle"); 
+    if (document.getElementById("selectTitle").value == "retro")
+        {
+                title.textContent = "Ye Olde Printus Pressus";
+                title.style.fontSize = "50px";
+                title.style.textDecoration = "underline";
+                title.style.color = localStorage.getItem("textColor");
+        }
+    if (document.getElementById("selectTitle").value == "futuristic")
+        {
+                title.textContent = "iJournal 2.0 [Beta]";
+                title.style.fontSize = "50px";
+                title.style.fontStyle = "italic";
+                title.style.color = localStorage.getItem("textColor");
+        }
+    if (document.getElementById("selectTitle").value == "normal")
+        {
+                title.textContent = "Journal Website";
+                title.style.fontSize = "50px";
+                title.style.fontStyle = "normal";
+                title.style.color = localStorage.getItem("textColor");
+        }
+        
+    localStorage.setItem("titleStyle", document.getElementById("selectTitle").value);
+}
+
+
+
+
+
+
+
+
+
