daa9160f4f04fca68e59c15615fd2cce925f13f0
Jack <fyruxis@gmail.com>
2017-05-29 10:15:38 +0930

    Unsure what was changed, committing to pull directory

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -5,7 +5,7 @@ script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-// controls the functionality of the next button
+// controls the functionality of the sign in button
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -13,13 +13,4 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   window.location.replace('main.html');
-}
-
-// controls the functionality of the register button
-function register() {
-  if (document.getElementsByName("task") == "Account Sign In") {
-    document.getElementsByName("task") = "Create New Account";
-    document.getElementsByName("forminput") = "";
-    document.getElementByName("next").style.visibility = "hidden";
-  }
 }
